4f7498a2e7e127209281a49353f3e816226a42d1
Virgil <benjamin.dromard@gmail.com>
2023-05-03 17:39:13 +0200

    Sauvegarde commentaires background.js

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
+// Importation du module File System de Node
 import * as fs from 'fs/promises';
 
+// Lecture du fichier blacklist.txt
 const file = await fs.open('blacklist.txt');
 
+// Déclaration d'un tableau vide pour stocker les URLs des sites à bloquer
 const defaultFilters = [];
 
+// Loop qui lit toutes les lignes du fichier txt et va push chacune des strings dans defaultFilters
 for await (const line of file.readLines()) {
     defaultFilters.push(line.trim())
 }
@@ -12,6 +16,7 @@ for await (const line of file.readLines()) {
 
 console.log(defaultFilters)
 
+// Bloquage des sites à partir de webRequest
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) { return { cancel: true }},
     { urls: defaultFilters },
