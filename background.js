d5aecae6ab2d05dfe8d317cdb365e85a0bc53f5c
Virgil <benjamin.dromard@gmail.com>
2023-05-03 16:32:04 +0200

    Sauvegarde script de contenu commenté

--- background.js
+++ background.js
@@ -2,20 +2,10 @@ import * as fs from 'fs/promises';
 
 const file = await fs.open('blacklist.txt');
 
-const elements = document.getElementsByTagName("*");
-const banniereMilieu = document.getElementById("banniere_milieu");
-
 const defaultFilters = [];
 
 for await (const line of file.readLines()) {
     defaultFilters.push(line.trim())
-}
-
-
-const ariaLabel = elements.getAttribute("aria-label");
-
-if (ariaLabel == "Advertisment"){
-    console.log("Ceci est une publicité")
 }
 
 // const googleId = document.getElementById("google_ads_iframe_/128139881/LM_lemonde/a_la_une/a_la_une/hp/banniere_milieu_0__container__")
