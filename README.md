4d2cf61eac1e2df6c1c0e29dc1a33ba8beeb778d
la2ure <2avlaure@gmail.com>
2023-05-04 14:54:22 +0200

    New branch, test insert picture on web page

--- README.md
+++
@@ -1,1 +0,0 @@
-Projet d'extension Google Chrome, créé par Hanâa Belhamidi, Laure Mestre et Benjamin Dromard.
--- content-script.js
+++ content-script.js
@@ -1,17 +1,32 @@
 // Variable qui sélectionne tous les éléments HTML de la page, renvoie un tableau.
 const elements = document.getElementsByTagName("*");
 // Variable qui sélectionne l'élement banniere_milieu sur Lemonde.fr
-const banniereMilieu = document.getElementById("banniere_milieu");
+// const title = document.getElementById("w3-bar w3-card-2 notranslate w3-white");
 // Variable pour side-section sur W3School
-const sideSection = document.getElementsByClassName("sidesection");
+function changeColor(){
+    let main = document.getElementById("main")
+    main.style.backgroundColor = "pink"
+    console.log('coucou')
+}
+changeColor()
 
-console.log(sideSection)
+// function loadPicture (){
+//     let pics = document.getElementById("mainLeaderboard")
+//     pics.style.backgroundColor = "red"
+// }
+// loadPicture()
+
+const img = document.createElement("img")
+img.src = "https://www.pinterest.fr/pin/263038434479600308/"
+src = doucment.getElementById("pagetop")
+src.style.backgroundImage(img)
+
 
 // Loop pour retrouver l'attribute aria-label ; s'il est égal à Advertisment, alors on le loggue dans la console.
-for (let i = 0 ; i < elements.length ; i++) {
-    let ariaLabel = elements[i].getAttribute("aria-label")
-    if (ariaLabel == "Advertisment"){
-    console.log("PUB !!!")
-}
-}
+// for (let i = 0 ; i < elements.length ; i++) {
+//     let ariaLabel = elements[i].getAttribute("aria-label")
+//     if (ariaLabel == "Advertisment"){
+//     console.log("PUB !!!")
+// }
+// }
 
