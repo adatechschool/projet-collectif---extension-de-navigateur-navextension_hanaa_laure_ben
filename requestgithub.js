b8d2b68129e1ab87d3edd8b33fd7355b2c48d94f
Virgil <benjamin.dromard@gmail.com>
2023-05-05 15:52:18 +0200

    Sauvegarde requestgithub.js fonctionnel

--- requestgithub.js
+++ requestgithub.js
@@ -1,13 +1,23 @@
 import { Octokit } from "octokit";
-import * as config from 'config.env';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs/promises';
+dotenv.config()
 
-const octokit = new Octokit({ process.env.config.USER_TOKEN });
+const octokit = new Octokit({ auth: process.env.USER_TOKEN });
 
-const response = await octokit.request("GET {owner}/{repo}/blob/master", {
+const response = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+    mediaType: {
+        format: "raw"
+    },
     owner: "notracking",
-    repo: "hosts-blocklists"
-});
+    repo: "hosts-blocklists",
+    path: "domains.txt"
+}
+)
+
+let file = await fs.writeFile('domains.txt', response.data)
 
 console.log(`Response status : ${response.status}`);
-// console.log(response)
+console.log(typeof response.data)
 
+// process.env.config.USER_TOKEN
