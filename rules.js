fd524abaf0945b6a6d60939f83d9dd08865e9e31
Virgil <benjamin.dromard@gmail.com>
2023-05-10 12:35:26 +0200

    WIP modification des dictionaires avec URL, logique à trouver

--- rules.js
+++ rules.js
@@ -1,8 +1,28 @@
+console.time()
+import { Octokit } from "octokit";
+import * as dotenv from 'dotenv';
 import * as fs from 'fs/promises';
-// import * as config from 'dotenv';
 
-const file = await fs.open('domains.txt');
-let idCount = 1;
+dotenv.config()
+
+// Instance d'Octokit, l'outil de l'API de GitHub, en s'authentifiant avec la variable d'environnement (dotenv).
+const octokit = new Octokit({ auth: process.env.USER_TOKEN });
+
+// Réponse de l'API de GitHub
+const response = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+    mediaType: {
+        format: "raw"
+    },
+    owner: "notracking",
+    repo: "hosts-blocklists",
+    path: "domains.txt"
+}
+)
+
+// Ecriture des données dans domains.txt, puis ouverture du fichier.
+let data = await fs.writeFile('domains.txt', response.data)
+let file = await fs.open('domains.txt');
+
 
 // Déclaration d'un tableau vide pour stocker les URLs des sites à bloquer
 let urlsArray = [];
@@ -10,7 +30,7 @@ let urlsArray = [];
 // Déclaration d'un tableau vide pour stocker les dictionaries à envoyer vers rules.json
 let dictionariesArray = [];
 
-let txtId = 0;
+// let txtId = 0;
 
 // Loop qui lit toutes les lignes du fichier txt et va push chacune des strings dans urlsArray
 for await (let line of file.readLines()) {
@@ -25,7 +45,7 @@ for await (let line of file.readLines()) {
 
 // Ajout dans dictionariesArray de chaque url présente dans blacklist.txt, 
 // en respectant le format du dictionnaire demandé par rules.json
-
+let idCount = 1;
 urlsArray.forEach(url => {
     let ruleDictionary = {
         id: parseInt(`${idCount++}`),
@@ -48,59 +68,45 @@ urlsArray.forEach(url => {
 
 // fs.writeFile('correctedDomains.json', JSON.stringify(dictionariesArray));
 
-let firstDictionary = [];
-let secondDictionary = [];
-let thirdDictionary = []; 
-let fourthDictionary = [];
-let fifthDictionary = [];
-let sixthDictionary = [];
-let seventhDictionary = [];
-let eigthDictionary = [];
-let ninthDictionary = [];
-let tenthDictionary = [];
+let dictionary1 = [];
+let dictionary2 = [];
+let dictionary3 = []; 
+let dictionary4 = [];
+let dictionary5 = [];
+let dictionary6 = [];
+let dictionary7 = [];
+let dictionary8 = [];
+let dictionary9 = [];
+let dictionary10 = [];
 
 // Comment faire en sorte de n'écrire que les 30000 premières URLs ?
 dictionariesArray.forEach(dictionary => {  
 switch (true) {
     case dictionary.id < 29999:
-        firstDictionary.push(dictionary);
-    case dictionary.id < 59999:
-        secondDictionary.push(dictionary);
-    case dictionary.id < 89999:
-        thirdDictionary.push(dictionary);
-    case dictionary.id < 119999:
-        fourthDictionary.push(dictionary);
-    case dictionary.id < 149999:
-        fifthDictionary.push(dictionary);
-    case dictionary.id < 179999:
-        sixthDictionary.push(dictionary);
-    case dictionary.id < 209999:
-        seventhDictionary.push(dictionary);
-    case dictionary.id < 239999:
-        eigthDictionary.push(dictionary);
-    case dictionary.id < 269999:
-        ninthDictionary.push(dictionary);
-    case dictionary.id < 299999:
-        tenthDictionary.push(dictionary)
-}}) 
+        dictionary1.push(dictionary);
+    case dictionary.id >= 30000 && dictionary.id <= 59999:
+        dictionary2.push(dictionary);
+    case dictionary.id >= 60000 && dictionary.id <= 89999:
+        dictionary3.push(dictionary);
+    case dictionary.id >= 90000 && dictionary.id <= 119999:
+        dictionary4.push(dictionary);
+    case dictionary.id >= 120000 && dictionary.id <= 149999:
+        dictionary5.push(dictionary);
+    case dictionary.id >= 150000 && dictionary.id <= 179999:
+        dictionary6.push(dictionary);
+    case dictionary.id >= 180000 && dictionary.id <= 209999:
+        dictionary7.push(dictionary);
+    case dictionary.id >= 210000 && dictionary.id <= 239999:
+        dictionary8.push(dictionary);
+    case dictionary.id >= 240000 && dictionary.id <= 269999:
+        dictionary9.push(dictionary);
+    case dictionary.id >= 270000 && dictionary.id <= 299999:
+        dictionary10.push(dictionary);
+}}); 
 
-fs.writeFile("rules1.json", JSON.stringify(firstDictionary), err => {
+for (let i = 1 ; i < 10 ; i++ ){
+fs.writeFile(`rules${i}.json`, JSON.stringify(eval(`dictionary${i}`)), err => {
     if (err) console.log("Error writing file:", err);})
-fs.writeFile("rules2.json", JSON.stringify(secondDictionary), err => {
-    if (err) console.log("Error writing file:", err);})
-fs.writeFile("rules3.json", JSON.stringify(thirdDictionary), err => {
-    if (err) console.log("Error writing file:", err);})
-fs.writeFile("rules4.json", JSON.stringify(fourthDictionary), err => {
-    if (err) console.log("Error writing file:", err);})
-fs.writeFile("rules5.json", JSON.stringify(fifthDictionary), err => {
-    if (err) console.log("Error writing file:", err);})
-fs.writeFile("rules6.json", JSON.stringify(sixthDictionary), err => {
-    if (err) console.log("Error writing file:", err);})
-fs.writeFile("rules7.json", JSON.stringify(seventhDictionary), err => {
-    if (err) console.log("Error writing file:", err);})
-fs.writeFile("rules8.json", JSON.stringify(eigthDictionary), err => {
-    if (err) console.log("Error writing file:", err);})
-fs.writeFile("rules9.json", JSON.stringify(ninthDictionary), err => {
-    if (err) console.log("Error writing file:", err);})
-fs.writeFile("rules10.json", JSON.stringify(tenthDictionary), err => {
-    if (err) console.log("Error writing file:", err);})
+};    
+
+console.timeEnd()
